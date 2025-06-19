b78f653a0d34aee7c416941bc5b56901f7acf8fe
RikitoMorikawa <161588113+RikitoMorikawa@users.noreply.github.com>
2025-06-19 23:51:16 +0900

    ＃

--- src/components/veficle-detail/page.tsx
+++ src/components/veficle-detail/page.tsx
@@ -177,6 +177,16 @@ const VehicleDetailComponent: React.FC<VehicleDetailComponentProps> = ({
                   </button>
                   {!isAdmin && (
                     <button
+                      onClick={() => onTabChange("見積書作成")}
+                      className={`px-6 py-4 text-center text-sm font-medium ${
+                        activeTab === "見積書作成" ? "border-b-2 border-red-600 text-red-600" : "text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                      }`}
+                    >
+                      見積書作成
+                    </button>
+                  )}
+                  {!isAdmin && (
+                    <button
                       onClick={() => onTabChange("ローン審査申込")}
                       className={`px-6 py-4 text-center text-sm font-medium ${
                         activeTab === "ローン審査申込" ? "border-b-2 border-red-600 text-red-600" : "text-gray-500 hover:text-gray-700 hover:border-gray-300"
